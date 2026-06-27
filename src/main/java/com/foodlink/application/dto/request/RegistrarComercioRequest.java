6d9477c5ac13a1a0ef5f9f49aee300e79e748eb8
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-06-27 18:04:07 -0500

    feat(Comercio): ✨ add RegistrarComercioRequest DTO

---
+++ src/main/java/com/foodlink/application/dto/request/RegistrarComercioRequest.java
@@ -0,0 +1,14 @@
+package com.foodlink.application.dto.request;
+
+public record RegistrarComercioRequest(
+        String ruc,
+        String nombre,
+        String telefono,
+        String email,
+        String provincia,
+        String ciudad,
+        String callePrincipal,
+        String calleSecundaria,
+        String referencia
+) {
+}
