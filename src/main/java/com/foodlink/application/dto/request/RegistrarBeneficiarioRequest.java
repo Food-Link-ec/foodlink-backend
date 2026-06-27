2693b336eb52f6804d03856a61634e1528d12238
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-06-27 18:04:07 -0500

    feat(Beneficiario): ✨ add RegistrarBeneficiarioRequest DTO

---
+++ src/main/java/com/foodlink/application/dto/request/RegistrarBeneficiarioRequest.java
@@ -0,0 +1,14 @@
+package com.foodlink.application.dto.request;
+
+public record RegistrarBeneficiarioRequest(
+        String nombre,
+        String ruc,
+        String email,
+        String telefono,
+        String provincia,
+        String ciudad,
+        String callePrincipal,
+        String calleSecundaria,
+        String referencia
+) {
+}
