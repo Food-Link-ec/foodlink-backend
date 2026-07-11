e9c5dbd3b865f8df40c9836e1d83629b4ec1ce35
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-07-10 23:18:30 -0500

    refactor(Comercio): ♻️ replace primitive strings with domain value objects

--- src/main/java/com/foodlink/domain/model/comercio/exception/RucInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package com.foodlink.domain.model.comercio.exception;
-
-public class RucInvalidoException extends RuntimeException {
-
-    public RucInvalidoException(String mensaje) {
-        super(mensaje);
-    }
-}
