27960ac7f49fabaa2822f4c69b228b4cddffba62
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-07-11 18:25:28 -0500

    feat(Lote): ✨ update PublicarLoteUseCase interface to include comercioId param

--- src/main/java/com/foodlink/domain/port/input/PublicarLoteUseCase.java
+++ src/main/java/com/foodlink/domain/port/input/PublicarLoteUseCase.java
@@ -3,7 +3,9 @@ package com.foodlink.domain.port.input;
 import com.foodlink.application.dto.request.PublicarLoteRequest;
 import com.foodlink.application.dto.response.LoteResponse;
 
+import java.util.UUID;
+
 public interface PublicarLoteUseCase {
 
-    LoteResponse publicar(PublicarLoteRequest request);
+    LoteResponse publicar(PublicarLoteRequest request, UUID comercioId);
 }
