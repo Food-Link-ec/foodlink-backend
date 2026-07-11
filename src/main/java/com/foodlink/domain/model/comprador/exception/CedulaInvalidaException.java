f1e630f928d3b9f9b764b4147074109c1daff437
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-07-10 23:18:36 -0500

    refactor(Comprador): ♻️ replace primitive strings with domain value objects

--- src/main/java/com/foodlink/domain/model/comprador/exception/CedulaInvalidaException.java
+++
@@ -1,8 +0,0 @@
-package com.foodlink.domain.model.comprador.exception;
-
-public class CedulaInvalidaException extends RuntimeException {
-
-    public CedulaInvalidaException(String mensaje) {
-        super(mensaje);
-    }
-}
