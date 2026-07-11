09f9049c193be50a5b3dfa13bd25f94e970b7f81
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-07-11 00:34:21 -0500

    feat(Lote): ✨ add EstadoLote and Modalidad enums

---
+++ src/main/java/com/foodlink/domain/model/lote/EstadoLote.java
@@ -0,0 +1,13 @@
+package com.foodlink.domain.model.lote;
+
+public enum EstadoLote {
+    BORRADOR,
+    PENDIENTE_VALIDACION,
+    DISPONIBLE,
+    RESERVADO,
+    VENDIDO,
+    DONADO,
+    ENTREGADO,
+    EXPIRADO,
+    RECHAZADO
+}
---
+++ src/main/java/com/foodlink/domain/model/lote/Modalidad.java
@@ -0,0 +1,7 @@
+package com.foodlink.domain.model.lote;
+
+public enum Modalidad {
+    VENTA,
+    DONACION,
+    RETIRO_DIRECTO
+}
