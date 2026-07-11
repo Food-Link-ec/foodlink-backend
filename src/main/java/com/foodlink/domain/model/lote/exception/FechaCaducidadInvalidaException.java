38b962cb3bc03d8a661a0341e8990e4679802d0b
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-07-11 00:34:24 -0500

    feat(Lote): ✨ add LoteNoDisponibleException and FechaCaducidadInvalidaException

---
+++ src/main/java/com/foodlink/domain/model/lote/exception/FechaCaducidadInvalidaException.java
@@ -0,0 +1,8 @@
+package com.foodlink.domain.model.lote.exception;
+
+public class FechaCaducidadInvalidaException extends RuntimeException {
+
+    public FechaCaducidadInvalidaException(String mensaje) {
+        super(mensaje);
+    }
+}
